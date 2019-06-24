36d8ef78e34929d0e15ec5ace32d87ff58694fcf
DanielGall500 <danielgallagher500@gmail.com>
2019-06-24 11:59:24 +0100

    Checks Validity Of Roster

--- ManagersDesktopApplication/uploadrosterdialog.cpp
+++ ManagersDesktopApplication/uploadrosterdialog.cpp
@@ -1,8 +1,8 @@
 #include "uploadrosterdialog.h"
 #include "ui_uploadrosterdialog.h"
 
-UploadRosterDialog::UploadRosterDialog(QWidget *parent) :
-    QDialog(parent),
+UploadRosterDialog::UploadRosterDialog(EmployeeDatabase *db, QWidget *parent) :
+    QDialog(parent), EMPL_DB(db),
     ui(new Ui::UploadRosterDialog)
 {
     ui->setupUi(this);
@@ -12,6 +12,8 @@ UploadRosterDialog::~UploadRosterDialog()
 {
     delete ui;
 }
+
+//Roster(string title, vectorStr2D csv_roster, col_pos names, row_pos dates, vectorStr non_shifts);
 
 void UploadRosterDialog::on_chooseFileButton_clicked()
 {
@@ -24,9 +26,62 @@ void UploadRosterDialog::on_chooseFileButton_clicked()
     ui->fileLabel->setText("File: " + f_dir);
 
     //Convert to STD String
-    std::string dir = qStr_to_stdStr(f_dir);
+    std::string file_dir = qStr_to_stdStr(f_dir);
 
-    //Create Roster
-    Roster rr(dir, ",", 0, true);
 
+    vectorStr2D file;
+    vectorStr to_ignore{"Department", "Floor"};
+    CSVReader rdr(file_dir, to_ignore, ",");
+
+    if(rdr.open_file())
+    {
+        file = rdr.read_csv();
+    }
+
+    //--TESTING--
+    row_pos dates_pos(0, 1);
+    col_pos names_pos(0, 1);
+    vectorStr non_shifts{"N/A", "Hol"};
+    string roster_title = "Week 1"; //MAKE UNIQUE ONLY
+    //-----------
+
+    rr = new Roster(roster_title, file, names_pos, dates_pos, non_shifts);
+
+    //Check roster is valid
+    string empl_not_found, error;
+    if(!rr->check_db_contains_rost_empl(*EMPL_DB, empl_not_found))
+    {
+        //Invalid Roster: Employee Not Found In Database
+
+        error = "Error: Employee " + empl_not_found
+                + " could not be found in the database.";
+
+        ui->uplRostOKButton->setEnabled(false);
+        ui->isValidRostLabel->setText(QString::fromStdString(error));
+    }
+    else if(!rr->check_formatted_correctly())
+    {
+        //Invalid Roster: Not Formatted Correctly
+
+        error = "Error: Roster Not Formatted Correctly";
+
+        ui->uplRostOKButton->setEnabled(false);
+        ui->isValidRostLabel->setText(QString::fromStdString(error));
+    }
+    else
+    {
+        //Valid Roster
+
+        ui->uplRostOKButton->setEnabled(true);
+        ui->isValidRostLabel->setText("Roster Valid");
+    }
+}
+
+void UploadRosterDialog::on_uplRostOKButton_clicked()
+{
+    //Upload the roster to the employee database
+    EMPL_DB->add_new_roster(*rr);
+
+    //Close the window
+    this->close();
 }
--- ManagersDesktopApplication/uploadrosterdialog.h
+++ ManagersDesktopApplication/uploadrosterdialog.h
@@ -6,6 +6,8 @@
 #include <QDir>
 #include "Roster/Roster.h"
 #include "globals.h"
+#include "EmployeeDatabase/EmployeeDatabase.h"
+#include "CSVReader/CSVReader.h"
 
 namespace Ui {
 class UploadRosterDialog;
@@ -15,12 +17,19 @@ class UploadRosterDialog : public QDialog
 {
     Q_OBJECT
 
+    EmployeeDatabase *EMPL_DB;
+
+    //Stores new roster
+    Roster *rr;
+
 public:
-    explicit UploadRosterDialog(QWidget *parent = nullptr);
+    explicit UploadRosterDialog(EmployeeDatabase *db, QWidget *parent = nullptr);
     ~UploadRosterDialog();
 
 private slots:
     void on_chooseFileButton_clicked();
+
+    void on_uplRostOKButton_clicked();
 
 private:
     Ui::UploadRosterDialog *ui;
