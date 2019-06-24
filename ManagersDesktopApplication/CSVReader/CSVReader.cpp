099bc7da116496aaed52b68957cdf5b8cc21e2a0
DanielGall500 <danielgallagher500@gmail.com>
2019-06-24 01:33:49 +0100

    Ignores lines with keywords

---
+++ ManagersDesktopApplication/CSVReader/CSVReader.cpp
@@ -0,0 +1,85 @@
+//
+//  CSVReader.cpp
+//  SwapShiftProject
+//
+//  Created by Daniel Gallagher on 22/06/2019.
+//  Copyright (c) 2019 Developer Co. All rights reserved.
+//
+
+#include "CSVReader.h"
+
+CSVReader::CSVReader(string dir, vectorStr str_to_ignore, string delim) :
+file_dir(dir), file_delim(delim), str_to_ignore(str_to_ignore)
+{}
+
+bool CSVReader::open_file()
+{
+    input_stream.open(file_dir, ios::in);
+    
+    if(!input_stream)
+    {
+        return false;
+    }
+    
+    return true;
+}
+
+vectorStr2D CSVReader::read_csv()
+{
+    vector<string> row;
+    string line;
+    bool ignore_line;
+        
+    while(getline(input_stream, line))
+    {
+        ignore_line = false;
+        row.clear();
+
+        //Check if this line should be ignored
+        for(string &s : str_to_ignore)
+        {
+            if(contains_str(line, s))
+            {
+                ignore_line = true;
+                break;
+            }
+        }
+
+        if(!ignore_line)
+        {
+            //Find the next delimeter
+            while (line.find(file_delim) != string::npos)
+            {
+                string entry = line.substr(0, line.find(file_delim));
+
+                row.push_back(entry);
+
+                line.erase(0, line.find(file_delim) + file_delim.length());
+            }
+
+            //Push the final part of our line back
+            row.push_back(line);
+
+
+            //Add new row to our vector
+            csv_vector.push_back(row);
+        }
+    }
+    
+    return csv_vector;
+    
+}
+
+bool CSVReader::contains_str(string orig, string substr)
+{
+    //Check if substr is contained within orig
+    if(orig.find(substr) != string::npos)
+        return true;
+
+    return false;
+}
+
+
+
+
+
---
+++ ManagersDesktopApplication/CSVReader/CSVReader.h
@@ -0,0 +1,50 @@
+//
+//  CSVReader.h
+//  SwapShiftProject
+//
+//  Created by Daniel Gallagher on 22/06/2019.
+//  Copyright (c) 2019 Developer Co. All rights reserved.
+//
+
+#ifndef __SwapShiftProject__CSVReader__
+#define __SwapShiftProject__CSVReader__
+
+#include <stdio.h>
+#include <string>
+#include <fstream>
+#include <vector>
+#include "globals.h"
+
+using namespace std;
+
+class CSVReader
+{
+    //File Directory & Delimeter
+    string file_dir,
+           file_delim;
+    
+    //Vector Which Holds CSV File
+    vectorStr2D csv_vector;
+    
+    //Stream From File
+    ifstream input_stream;
+
+    //Ignore any lines which contain the following strings
+    vectorStr str_to_ignore;
+
+    //Check if a string is a substring of another
+    bool contains_str(string orig, string substr);
+    
+    
+public:
+    CSVReader(string dir, vectorStr str_to_ignore, string file_delim = ",");
+    
+    //Check If File Opens
+    bool open_file();
+    
+    //Read CSV File
+    vectorStr2D read_csv();
+    
+};
+
+#endif /* defined(__SwapShiftProject__CSVReader__) */
